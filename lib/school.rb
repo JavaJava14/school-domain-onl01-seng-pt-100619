d542f7ac1ebdd04b3d81d9f9f267c612549e5659
Jose Chavira <joeychavira2016@gmail.com>
2019-10-20 01:41:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,7 +18,7 @@ class School
   def sort
     sorted = {}
     roster.each do |grade, students|
-      sorted[grade] = students.sorted
+      sorted[grade] = students.sort
     end
     sorted
   end
